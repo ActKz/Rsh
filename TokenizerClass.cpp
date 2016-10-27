c3ca2d66e47f389ec14dc203bc6abb87ba424972
actkz <actkz.cs02@g2.nctu.edu.tw>
2016-10-27 20:46:10 +0800

    Finish TokenizerClass and the testing seems great
    '

--- TokenizerClass.cpp
+++ TokenizerClass.cpp
@@ -3,33 +3,42 @@
 #include <string>
 #include <queue>
 #include <vector>
+#include <cstdio>
 #include <cstdlib>
+#include <string.h>
+#include <iostream>
 
 using namespace std;
 
 typedef struct GROUP_TOKEN{
     int st;
-    string *cmd;
+    string file_name;
     vector<string> argv;
 }group_token;
 
-Tokenizer::Tokenizer(char *input){
-   _buf = input;
+Tokenizer::Tokenizer(const char *input){
+   _buf = strdup(input);
    _tokens = new group_token;
    _delay_pipe = 0;
 }
 
-queue<struct GROUP_TOKEN> Tokenizer::cmd_args(){
+Tokenizer::~Tokenizer(){
+    _tokens->~GROUP_TOKEN();
+}
+
+queue<struct GROUP_TOKEN*> Tokenizer::cmd_args(){
     return _cmd_args;
 }
 
 void Tokenizer::parse(){
     _cptr = strtok(_buf, " ");
     while (_cptr != NULL) {
-
+      if(_tokens->st == WRITE_FILE){
+          _tokens->file_name = strdup(_cptr);
+          break;
+      }
       switch (_cptr[0]) {
       case '|':
-
         if (isdigit(_cptr[1])) {
           _tokens->st = PIPE_n;
           _delay_pipe = atoi(_cptr);
@@ -41,14 +50,14 @@ void Tokenizer::parse(){
         }
         _cmd_args.push(_tokens);
         _tokens = new group_token;
-
         break;
       case '!':
         if (isdigit(_cptr[1])) {
           _tokens->st = SUPERPIPE_n;
           _delay_pipe = atoi(_cptr);
-        } else
+        } else{
           _tokens->st = SUPERPIPE_n_ERROR;
+        }
         _cmd_args.push(_tokens);
         break;
       case '>':
@@ -61,3 +70,22 @@ void Tokenizer::parse(){
     }
     _cmd_args.push(_tokens);
 }
+
+#ifdef test
+int main(){
+    char input[15000];
+    fgets(input,15000,stdin);
+    Tokenizer tmp(input);
+    tmp.parse();
+    queue<struct GROUP_TOKEN*> token = tmp.cmd_args();
+    while(!token.empty()){
+        cout<<"st: "<<token.front()->st<<", argv: ";
+        for(int i=0;i<token.front()->argv.size();i++)
+            cout<<token.front()->argv[i]<<" ";
+        cout<<", write to: "<<token.front()->file_name;
+        cout<<endl;
+        token.pop();
+    }
+    cout<<"END"<<endl;
+}
+#endif
--- TokenizerClass.h
+++ TokenizerClass.h
@@ -1,6 +1,6 @@
-#include <string>
-#include <queue>
-#include <vector>
+#include<string>
+#include<queue>
+#include<vector>
 using namespace std;
 
 struct GROUP_TOKEN;
@@ -8,16 +8,16 @@ struct GROUP_TOKEN;
 class Tokenizer{
 
 public:
-    Tokenizer(char*);
+    Tokenizer(const char*);
     ~Tokenizer();
 
-    queue<struct GROUP_TOKEN> cmd_args();
+    queue<struct GROUP_TOKEN*> cmd_args();
     void parse();
 
 private:
     int _delay_pipe;
     char *_cptr, *_buf;
     struct GROUP_TOKEN *_tokens;
-    queue<struct GROUP_TOKEN> _cmd_args;
+    queue<struct GROUP_TOKEN*> _cmd_args;
 
 };
